b104b1a1f966951d69d45def324e817eef276c27
Ekram-ekiti <ekru6482@gmail.com>
2025-04-10 21:28:19 +0300

    making responsive

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,26 @@
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <header className="bg-header-bg absolute z-[999] w-full text-white px-6 py-4 flex items-center justify-between">
+    <header className="bg-header-bg absolute z-[999] w-full text-white px-4 sm:px-6 py-4 flex items-center justify-between">
       {/* Logo */}
-      <div className="flex items-center ml-[40px]">
+      <div className="flex items-center">
         <img
-          src="/logo.png" // Corrected to directly reference the public folder
+          src="/logo.png"
           alt="Rebel Rover Logo"
-          className="w-[300px] sm:w-[357px] mr-4"
+          className="w-[180px] sm:w-[220px] md:w-[300px] lg:w-[357px] mr-4"
         />
       </div>
 
       {/* Navigation */}
-      <nav className="hidden md:flex items-center gap-x-[50px] mr-[40px] text-sm font-medium">
+      <nav className="hidden md:flex items-center gap-x-6 lg:gap-x-[50px] text-sm font-medium">
         <a href="#" className="hover:text-gray-300">
           Home
         </a>
@@ -34,7 +41,6 @@ const Header = () => {
           </div>
         </div>
 
-        {/* Destination */}
         <a href="#" className="hover:text-gray-300">
           Destination
         </a>
@@ -52,14 +58,65 @@ const Header = () => {
           </div>
         </div>
 
-        {/* Contact */}
         <a href="#" className="hover:text-gray-300">
           Contact
         </a>
       </nav>
 
+      {/* Mobile Hamburger Icon */}
+      <div className="md:hidden flex items-center">
+        <button
+          onClick={toggleMobileMenu}
+          className="bg-gray-600 p-2 rounded-full text-white hover:bg-gray-700"
+        >
+          <FaBars size={20} />
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {isMobileMenuOpen && (
+        <div className="absolute top-16 right-0 w-full bg-gray-800 text-white md:hidden">
+          <div className="flex flex-col items-center py-4">
+            <a href="#" className="py-2 hover:text-gray-300">
+              Home
+            </a>
+            <a href="#" className="py-2 hover:text-gray-300">
+              About Us
+            </a>
+            <div className="relative group">
+              <div className="py-2 cursor-pointer">Package</div>
+              <div className="absolute hidden group-hover:block bg-gray-700 text-white rounded shadow-lg p-2 mt-2 min-w-[140px] z-10">
+                <a href="#" className="block px-2 py-1 hover:text-gray-300">
+                  Package 1
+                </a>
+                <a href="#" className="block px-2 py-1 hover:text-gray-300">
+                  Package 2
+                </a>
+              </div>
+            </div>
+            <a href="#" className="py-2 hover:text-gray-300">
+              Destination
+            </a>
+            <div className="relative group">
+              <div className="py-2 cursor-pointer">Blog</div>
+              <div className="absolute hidden group-hover:block bg-gray-700 text-white rounded shadow-lg p-2 mt-2 min-w-[140px] z-10">
+                <a href="#" className="block px-2 py-1 hover:text-gray-300">
+                  Latest Posts
+                </a>
+                <a href="#" className="block px-2 py-1 hover:text-gray-300">
+                  Travel Tips
+                </a>
+              </div>
+            </div>
+            <a href="#" className="py-2 hover:text-gray-300">
+              Contact
+            </a>
+          </div>
+        </div>
+      )}
+
       {/* Search Icon */}
-      <div className="ml-4 mr-[46px] cursor-pointer hover:text-gray-300">
+      <div className="ml-2 sm:ml-4 cursor-pointer hover:text-gray-300">
         <FaSearch size={18} />
       </div>
     </header>
