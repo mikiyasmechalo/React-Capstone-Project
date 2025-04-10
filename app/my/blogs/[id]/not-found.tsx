3b04e215e7f92f13642f2eb41617758182b8b8cb
mikiyasmechalo <mikiyasmechalo70@gmail.com>
2025-04-10 18:35:50 +0300

    added a proper error page and MY: added a not-found page for unavailable blogs.

--- app/my/blogs/[id]/not-found.tsx
+++ app/my/blogs/[id]/not-found.tsx
@@ -1,26 +1,25 @@
-"use client";
 import Button from "@/components/Button";
 import Link from "next/link";
 import React from "react";
 import { FaFaceFrown } from "react-icons/fa6";
 
-const error = () => {
+const notFound = () => {
   return (
     <main
       className="flex lg:mx-50 lg:mt-90 h-fit
           md:m-25 md:mt-50 flex-col items-center justify-center gap-3 mt-35"
     >
       <FaFaceFrown className="size-15 text-gray-400" />
-      <h2 className="text-xl font-semibold">Error Occured</h2>
-      <p className="text-center">Page not found</p>
+      <h2 className="text-xl font-semibold">404 Not Found</h2>
+      <p className="text-center">Could not find the requested Blog.</p>
       <Link
         href="/my/blogs"
         className="mt-4 rounded-md px-4 py-2 text-sm text-white"
       >
-        <Button>Go to Home</Button>
+        <Button>Go Back</Button>
       </Link>
     </main>
   );
 };
 
-export default error;
+export default notFound;
