fefd59564c7625172b45f7630a7fc68dc3324ad8
mikiyasmechalo <mikiyasmechalo70@gmail.com>
2025-04-14 14:15:02 +0300

    Added a page to view and, edit and add destinations from the API

--- app/destinations/page.tsx
+++ app/destinations/page.tsx
@@ -1,75 +1,469 @@
 "use client";
+import Button from "@/components/ui/Button";
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-import React, { Suspense } from "react";
+import Link from "next/link";
+import React, { useCallback, useEffect, useState } from "react";
+import { FaChevronRight } from "react-icons/fa6";
 
-// Create a separate component for the dynamic text
-const BookingConfirmationText = () => {
-  const searchParams = useSearchParams();
-  const location = searchParams.get("location");
-  const date = searchParams.get("date");
-  const people = searchParams.get("people");
+interface Destination {
+  id: string;
+  name: string;
+  country: string;
+  price: string;
+  image: string;
+  description: string;
+}
 
+const DestinationSkeleton = () => {
   return (
-    <p className="md:text-2xl text-lg flex flex-col items-center gap-2 text-center">
-      You have booked a package
-      {location && (
-        <>
-          <br />
-          for {location}
-        </>
-      )}
-      {date && (
-        <>
-          <br />
-          on {date}
-        </>
-      )}
-      {people && (
-        <>
-          <br />
-          for {people} {people === "1" ? "person" : "people"}.
-        </>
-      )}
-    </p>
+    Array.from({ length: 10 }).map((_, i) => (
+      <div
+        key={i}
+        className={`animate-pulse relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
+      >
+        <div className="h-6 bg-gray-200 rounded w-full mb-2"></div>
+        <div className="h-6 bg-gray-200 rounded w-full mb-2"></div>
+        <div className="h-6 bg-gray-200 rounded w-full mb-2"></div>
+        <div className="h-6 bg-gray-200 rounded w-full mb-2"></div>
+        <div className="h-6 bg-gray-200 rounded w-full mb-4"></div>
+        <div className="mt-4 space-x-2 flex">
+          <div className="h-8 bg-gray-200 rounded w-20"></div>
+          <div className="h-8 bg-gray-200 rounded w-20"></div>
+        </div>
+      </div>
+    ))
   );
 };
 
-const Page = () => {
+const DestinationList = ({
+  destinations,
+  onEdit,
+  onDelete,
+  truncateDescription
+}: {
+  destinations: Destination[];
+  onEdit: (destination: Destination) => void;
+  onDelete: (id: string) => void;
+  truncateDescription: (text: string, maxLength?: number) => string;
+}) => {
   return (
-    <div>
-      <main className="relative ">
+    <>
+      {destinations.map((item) => (
         <div
-          className="relative bg-cover bg-center bg-no-repeat lg:min-h-[1160px] md:min-h-[600px] sm:min-h-[400px] min-h-[300px]" // Added responsive height classes
+          key={item.id}
+          className=" p-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 flex flex-col gap-2 justify-between items-start xs:w-80"
         >
+          <div>
+            <p>
+              <span className="font-semibold">Name:</span> {item.name}
+            </p>
+            <p>
+              <span className="font-semibold">Country:</span> {item.country}
+            </p>
+            <p>
+              <span className="font-semibold">Price:</span> {item.price}
+            </p>
+            <p>
+              <span className="font-semibold">Image:</span> {item.image}
+            </p>
+            <p>
+              <span className="font-semibold">Description:</span>{" "}
+              {truncateDescription(item.description)}
+            </p>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-2 w-full">
+            <Button
+              onClick={() => onEdit(item)}
+              className="bg-yellow-500 text-white hover:bg-yellow-600 text-lg! p-2! px-6!"
+              size="sm"
+            >
+              Edit
+            </Button>
+            <Button
+              onClick={() => onDelete(item.id)}
+              className="bg-red-500 text-white hover:bg-red-600  text-lg! p-2! px-4!"
+              size="sm"
+            >
+              Delete
+            </Button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+const page = () => {
+  const [formData, setFormData] = useState<Destination>({
+    id: "",
+    name: "",
+    country: "",
+    price: "",
+    image: "",
+    description: "",
+  });
+
+  const [apiResponse, setApiResponse] = useState<Destination[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [editId, setEditId] = useState<string | null>(null);
+  const [showForm, setShowForm] = useState<boolean>(false);
+
+  const fetchDestinations = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://67eadc5834bcedd95f64c9f3.mockapi.io/RebelRover/Destinations"
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data: Destination[] = await response.json();
+      setApiResponse(data);
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchDestinations();
+  }, [fetchDestinations]);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      if (editId) {
+        const response = await fetch(
+          `https://67eadc5834bcedd95f64c9f3.mockapi.io/RebelRover/Destinations/${editId}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const updatedDestination: Destination = await response.json();
+
+        setApiResponse((prev) =>
+          prev
+            ? prev.map((dest) =>
+                dest.id === updatedDestination.id ? updatedDestination : dest
+              )
+            : [updatedDestination]
+        );
+        setEditId(null);
+      } else {
+        const response = await fetch(
+          "https://67eadc5834bcedd95f64c9f3.mockapi.io/RebelRover/Destinations",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const newDestination: Destination = await response.json();
+        setApiResponse((prev) =>
+          prev ? [...prev, newDestination] : [newDestination]
+        );
+      }
+
+      setFormData({
+        id: "",
+        name: "",
+        country: "",
+        price: "",
+        image: "",
+        description: "",
+      });
+      setShowForm(false);
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDelete = async (id: string) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        `https://67eadc5834bcedd95f64c9f3.mockapi.io/RebelRover/Destinations/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      setApiResponse((prev) => prev?.filter((dest) => dest.id !== id) || []);
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleEdit = (destination: Destination) => {
+    setFormData(destination);
+    setEditId(destination.id);
+    setShowForm(true);
+  };
+
+  const filteredDestinations = apiResponse
+    ? apiResponse.filter(
+        (dest) =>
+          dest.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          dest.country.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : [];
+
+  const truncateDescription = (text: string, maxLength = 100) => {
+    if (!text) return "";
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + "...";
+    }
+    return text;
+  };
+  return (
+    <>
+      <div className="md:min-h-screen bg-gray-100">
+        <div className="relative bg-cover bg-center bg-no-repeat lg:min-h-[1160px] md:min-h-[600px] sm:min-h-[400px] min-h-[300px]">
           <Image
-            src="/packages-cover.png"
-            alt="Blog Cover"
+            src="/blog-images/africa-heart.jpg"
+            alt="Contact Background"
             layout="fill"
             className="object-center object-cover"
             priority
           />
           <div className="absolute inset-0 bg-black opacity-30"></div>
-        </div>
-        <div
-          className="absolute top-0 left-0 z-10 text-center lg:my-130 h-fit
-        md:mt-90 flex flex-col m-auto items-center justify-center gap-10 sm:mt-50 mt-30 inset-0"
-        >
-          <div
-            className="absolute top-1/2 left-1/2 transform justify-center mt-8 md:mt-0 sm:gap-10
-        flex flex-col items-center -translate-x-1/2 -translate-y-1/2 text-white text-center"
-          >
-            <h1 className="md:text-7xl text-4xl font-bold mb-4 sm:text-nowrap">
-              Congratulations!
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center flex flex-col items-center">
+            <h1 className="text-4xl md:text-7xl font-bold mb-4">
+              Destinations
             </h1>
-            <Suspense fallback={<p className="text-white">Loading...</p>}>
-              <BookingConfirmationText />
-            </Suspense>
+            <p className="text-lg md:text-2xl flex items-center gap-2">
+              <Link
+                href="/my"
+                className="hover:text-gray-300 transition-colors"
+              >
+                Home
+              </Link>
+              <FaChevronRight />
+              <Link href="#" className="hover:text-gray-300 transition-colors">
+                Destinations
+              </Link>
+            </p>
           </div>
         </div>
-      </main>
-    </div>
+      </div>
+      <div className="container mx-auto p-4 flex flex-col gap-5">
+        <h1 className="md:text-6xl sm:text-4xl text-2xl font-bold mb-6 text-center text-gray-800">
+          Destinations
+        </h1>
+
+        <Button
+          onClick={() => setShowForm(!showForm)}
+          className="bg-blue-500 hover:bg-blue-600 max-w-70"
+          size="sm"
+        >
+          {showForm ? "Hide Form" : "Add Destination"}
+        </Button>
+
+        {showForm && (
+          <div className="mb-8 shadow-lg rounded-md border bg-white">
+            <div className="p-4">
+              <h3 className="text-lg font-semibold">Destination Form</h3>
+              <p className="text-sm text-gray-500">
+                Fill the form below to add a new destination.
+              </p>
+            </div>
+            <div className="p-4">
+              <form onSubmit={handleSubmit} className="space-y-6">
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                    placeholder="Enter destination name"
+                    className="mt-1 border rounded px-3 py-2 w-full"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="country"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Country
+                  </label>
+                  <input
+                    type="text"
+                    id="country"
+                    name="country"
+                    value={formData.country}
+                    onChange={handleChange}
+                    required
+                    placeholder="Enter country"
+                    className="mt-1 border rounded px-3 py-2 w-full"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="price"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Price
+                  </label>
+                  <input
+                    type="text"
+                    id="price"
+                    name="price"
+                    value={formData.price}
+                    onChange={handleChange}
+                    required
+                    placeholder="Enter price"
+                    className="mt-1 border rounded px-3 py-2 w-full"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="image"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Image
+                  </label>
+                  <input
+                    type="text"
+                    id="image"
+                    name="image"
+                    value={formData.image}
+                    onChange={handleChange}
+                    required
+                    placeholder="Enter image URL"
+                    className="mt-1 border rounded px-3 py-2 w-full"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="description"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Description
+                  </label>
+                  <textarea
+                    id="description"
+                    name="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                    required
+                    placeholder="Enter description"
+                    className="mt-1 border rounded px-3 py-2 w-full"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="px-4 py-2 rounded bg-blue-500 text-white w-full hover:bg-blue-600"
+                >
+                  {loading ? "Loading..." : editId ? "Update" : "Submit"}
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Search destinations..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="ring ring-black rounded-full px-4 py-3 w-full"
+          />
+        </div>
+
+        {error && (
+          <div className="p-4 rounded-md bg-red-100 text-red-800 border border-red-300">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="h-4 w-4 text-red-500 inline-block mr-2"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <line x1="12" y1="8" x2="12" y2="12" />
+              <line x1="12" y1="16" x2="12.01" y2="16" />
+            </svg>
+            <h4 className="text-lg font-semibold inline-block">Error</h4>
+            <p className="text-sm">{error}</p>
+          </div>
+        )}
+
+        {/* Display Destinations */}
+        <div className="shadow-lg rounded-md border bg-white flex justify-center flex-col items-center">
+          <div className="p-4 ">
+            <h3 className="text-lg font-semibold text-center">
+              All Destinations
+            </h3>
+            <p className="text-sm text-gray-500 text-center">
+              All Destinations from the API is shown below.
+            </p>
+          </div>
+          <div className="p-4 flex flex-wrap justify-center gap-4">
+            {loading ? (
+              <DestinationSkeleton />
+            ) : filteredDestinations.length > 0 ? (
+              <DestinationList
+                destinations={filteredDestinations.reverse()}
+                onDelete={handleDelete}
+                onEdit={handleEdit}
+                truncateDescription={truncateDescription}
+              />
+            ) : (
+              <div>No matching destinations found.</div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default Page;
+export default page;
+
